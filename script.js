c1986add7025ee3096754fe2b326f95bdbd98c25
unknown <habibhaseeb716@gmail.com>
2026-09-18 20:18:01 +0500

    Added Methods

--- script.js
+++ script.js
@@ -6,11 +6,42 @@
 // button1.addEventListener('click', showAlert);
 
 
-//      Second Method
+//     Method 2
 
-function ActionClick() {
-    let clickedButton = document.getElementById('button');
+// function ActionClick() {
+//     let clickedButton = document.getElementById('button');
 
-    clickedButton.onclick = () => (window.alert("you clicked the button"));
-}
-ActionClick();
+//     clickedButton.onclick = () => (window.alert("you clicked the button"));
+// }
+// ActionClick();
+
+
+//        Method 3
+// let ActionButton = document.querySelector('button');
+
+// let isClicked = false;
+
+// ActionButton.addEventListener('click', function(){
+//     if(!isClicked) 
+//         {
+//             window.alert("You Clicked the Button.")
+//         }
+        
+//         else {
+//             console.log("Button is not clicked!")
+//         }
+//     });
+
+//         Method 4
+
+// let ActionButton = document.querySelector('button');
+// let isClicked = false;
+// ActionButton.addEventListener('click', ()=> { if(isClicked === false)
+//     {
+//         window.alert("you Clicked")
+//     }
+//     else {
+//         console ("Error");
+//     }
+//  }
+// )
