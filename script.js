966c39846763a290d4660a9fdb9d08b4c8ef1afc
unknown <habibhaseeb716@gmail.com>
2026-09-18 12:49:22 +0500

    Second method in Web dev

--- script.js
+++ script.js
@@ -1,6 +1,16 @@
-let button1 = document.getElementById('button');
+//     Method 1
+// let button1 = document.getElementById('button');
  
-const showAlert = () => alert("Wrong Input please try again later.");
+// const showAlert = () => alert("Wrong Input please try again later.");
 
-button1.addEventListener('click', showAlert);
-let r = ""
+// button1.addEventListener('click', showAlert);
+
+
+//      Second Method
+
+function ActionClick() {
+    let clickedButton = document.getElementById('button');
+
+    clickedButton.onclick = () => (window.alert("you clicked the button"));
+}
+ActionClick();
