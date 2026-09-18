1ddc03c16394e5dc09400fc4870793c9c9fc4d84
unknown <habibhaseeb716@gmail.com>
2026-09-18 20:19:24 +0500

    Remove comments in js

--- script.js
+++ script.js
@@ -1,47 +1,47 @@
 //     Method 1
-// let button1 = document.getElementById('button');
+let button1 = document.getElementById('button');
  
-// const showAlert = () => alert("Wrong Input please try again later.");
+const showAlert = () => alert("Wrong Input please try again later.");
 
-// button1.addEventListener('click', showAlert);
+button1.addEventListener('click', showAlert);
 
 
 //     Method 2
 
-// function ActionClick() {
-//     let clickedButton = document.getElementById('button');
+function ActionClick() {
+    let clickedButton = document.getElementById('button');
 
-//     clickedButton.onclick = () => (window.alert("you clicked the button"));
-// }
-// ActionClick();
+    clickedButton.onclick = () => (window.alert("you clicked the button"));
+}
+ActionClick();
 
 
 //        Method 3
-// let ActionButton = document.querySelector('button');
+let ActionButton = document.querySelector('button');
 
-// let isClicked = false;
+let isClicked = false;
 
-// ActionButton.addEventListener('click', function(){
-//     if(!isClicked) 
-//         {
-//             window.alert("You Clicked the Button.")
-//         }
+ActionButton.addEventListener('click', function(){
+    if(!isClicked) 
+        {
+            window.alert("You Clicked the Button.")
+        }
         
-//         else {
-//             console.log("Button is not clicked!")
-//         }
-//     });
+        else {
+            console.log("Button is not clicked!")
+        }
+    });
 
-//         Method 4
+        Method 4
 
-// let ActionButton = document.querySelector('button');
-// let isClicked = false;
-// ActionButton.addEventListener('click', ()=> { if(isClicked === false)
-//     {
-//         window.alert("you Clicked")
-//     }
-//     else {
-//         console ("Error");
-//     }
-//  }
-// )
+let ActionButton = document.querySelector('button');
+let isClicked = false;
+ActionButton.addEventListener('click', ()=> { if(isClicked === false)
+    {
+        window.alert("you Clicked")
+    }
+    else {
+        console ("Error");
+    }
+ }
+)
